3b541d339117a94be8d9c50e7e82ad98e3f80999
Cande Djalo <cande@exemplo.com>
2026-05-12 08:30:26 -0300

    Fix: Forçando nova pasta motor-zap para o Chrome

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -4,6 +4,6 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Mudámos o nome para não ter ponto. O Render não vai apagar isto!
-  cacheDirectory: join(__dirname, 'chrome-cache'),
+  // Nome novo para forçar o Render a baixar TUDO de novo sem ler o cache corrompido!
+ cacheDirectory: join(__dirname, 'motor-zap'),
 };
